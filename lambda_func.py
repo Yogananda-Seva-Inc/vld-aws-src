e2612913a0e41538a7d5ece8743827051062f2c8
Andre Marquand <amarquand@gmail.com>
2025-01-26 09:32:30 +0100

    new update test

--- lambda_func.py
+++ lambda_func.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO: implement
+    # TODO: implement from console
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! updated from github ')
