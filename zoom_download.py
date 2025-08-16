16deb1de457765fa9e2b21e11aed60a98911f888
Andre Marquand <amarquand@gmail.com>
2025-08-16 13:43:25 +0200

    stage 2 testing zoom download

--- zoom_download.py
+++ zoom_download.py
@@ -143,148 +143,149 @@ if __name__ == "__main__":
     df_uuid.to_csv('./zoom_etl/'+date_time +
                    '/fact_uuid_etl_status_'+date_time+'.csv', index=False)
     print('df_master_data shape:', df_master_data.shape)
-    # ##########################################################################################
-    # # Select required columns and re-order in the Connection Data file
-    # ##########################################################################################
-    # columns_add = ['error_code', 'error', 'in_room_participants']
-    # for newcol in columns_add:
-    #     if newcol not in df_master_data.columns:
-    #         new_cols = [newcol]
-    #         df_master_data = df_master_data.reindex(
-    #             columns=df_master_data.columns.tolist() + new_cols)   # add empty cols
-    #     else:
-    #         pass
+    ##########################################################################################
+    # Select required columns and re-order in the Connection Data file
+    ##########################################################################################
+    columns_add = ['error_code', 'error', 'in_room_participants']
+    for newcol in columns_add:
+        if newcol not in df_master_data.columns:
+            new_cols = [newcol]
+            df_master_data = df_master_data.reindex(
+                columns=df_master_data.columns.tolist() + new_cols)   # add empty cols
+        else:
+            pass
 
-    # df_master_data = df_master_data[['id', 'user_id', 'user_name', 'ip_address', 'location', 'network_type', 'data_center',
-    #                                  'connection_type', 'join_time', 'leave_time', 'share_application', 'share_desktop',
-    #                                  'share_whiteboard', 'recording', 'pc_name', 'domain', 'harddisk_id', 'version', 'leave_reason',
-    #                                  'email', 'status', 'microphone', 'speaker', 'camera', 'meeting_id', 'uuid', 'error_code',
-    #                                  'error', 'in_room_participants']]
+    df_master_data = df_master_data[['id', 'user_id', 'user_name', 'ip_address', 'location', 'network_type', 'data_center',
+                                     'connection_type', 'join_time', 'leave_time', 'share_application', 'share_desktop',
+                                     'share_whiteboard', 'recording', 'pc_name', 'domain', 'harddisk_id', 'version', 'leave_reason',
+                                     'email', 'status', 'microphone', 'speaker', 'camera', 'meeting_id', 'uuid', 'error_code',
+                                     'error', 'in_room_participants']]
 
-    # ##########################################################################################
-    # print('After in_room_participants df_master_data shape:', df_master_data.shape)
-    # df_master_data['updated_at'] = date_time
-    # df_master_data.to_csv('./zoom_etl/'+date_time +
-    #                       '/fact_connections_raw_'+date_time+'.csv', index=False)
-    # print('Fact_Connections Successful')
-    # # fact_uuid_old['updated_at'] = fact_uuid_old['end_time'].max()
-    # df_uuid_new_only['updated_at'] = date_time
-    # fact_uuid = pd.concat([fact_uuid_old, df_uuid_new_only])
-    # fact_uuid.to_csv('./zoom_etl/'+date_time +
-    #                  '/fact_uuid_master_'+date_time+'.csv', index=False)
-    # fact_uuid.to_csv('./Production_Fact_UUID/fact_uuid_master_' +
-    #                  date_time+'.csv', index=False)
-    # # fact_uuid.to_csv('./Staging_Fact_UUID/fact_uuid_master.csv', index= False)
-    # df_uuid_new_only['duration'] = ((pd.to_datetime(df_uuid_new_only['end_time'], infer_datetime_format=True) -
-    #                                 pd.to_datetime(df_uuid_new_only['start_time'], infer_datetime_format=True)).dt.seconds/60).astype(int)
-    # df_uuid_new_only.to_csv(
-    #     './Staging_Fact_UUID/fact_uuid_increment.csv', index=False)
-    # ## If file exists, delete it ##
-    # myfile = "fact_uuid_master.csv"
-    # if os.path.isfile(myfile):
-    #     os.remove(myfile)
-    #     fact_uuid.to_csv('fact_uuid_master.csv', index=False)
-    # else:  # Show an error ##
-    #     print("Error: %s file not found" % myfile)
-    # fact_uuid_old.to_csv('./zoom_etl/'+date_time +
-    #                      '/fact_uuid_old_'+date_time+'.csv', index=False)
-    # df_uuid_new_only.to_csv('./zoom_etl/'+date_time +
-    #                         '/fact_uuid_new_'+date_time+'.csv', index=False)
-    # print('Fact_UUIDs Successful')
-    # end = time.time()
-    # hours, rem = divmod(end-start, 3600)
-    # minutes, seconds = divmod(rem, 60)
-    # print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+    ##########################################################################################
+    print('After in_room_participants df_master_data shape:', df_master_data.shape)
+    df_master_data['updated_at'] = date_time
+    df_master_data.to_csv('./zoom_etl/'+date_time +
+                          '/fact_connections_raw_'+date_time+'.csv', index=False)
+    print('Fact_Connections Successful')
+    # fact_uuid_old['updated_at'] = fact_uuid_old['end_time'].max()
+    df_uuid_new_only['updated_at'] = date_time
+    fact_uuid = pd.concat([fact_uuid_old, df_uuid_new_only])
+    fact_uuid.to_csv('./zoom_etl/'+date_time +
+                     '/fact_uuid_master_'+date_time+'.csv', index=False)
+    fact_uuid.to_csv('./Production_Fact_UUID/fact_uuid_master_' +
+                     date_time+'.csv', index=False)
+    # fact_uuid.to_csv('./Staging_Fact_UUID/fact_uuid_master.csv', index= False)
+    df_uuid_new_only['duration'] = ((pd.to_datetime(df_uuid_new_only['end_time'], infer_datetime_format=True) -
+                                    pd.to_datetime(df_uuid_new_only['start_time'], infer_datetime_format=True)).dt.seconds/60).astype(int)
+    df_uuid_new_only.to_csv(
+        './Staging_Fact_UUID/fact_uuid_increment.csv', index=False)
+    ## If file exists, delete it ##
+    myfile = "fact_uuid_master.csv"
+    if os.path.isfile(myfile):
+        os.remove(myfile)
+        fact_uuid.to_csv('fact_uuid_master.csv', index=False)
+    else:  # Show an error ##
+        print("Error: %s file not found" % myfile)
+    fact_uuid_old.to_csv('./zoom_etl/'+date_time +
+                         '/fact_uuid_old_'+date_time+'.csv', index=False)
+    df_uuid_new_only.to_csv('./zoom_etl/'+date_time +
+                            '/fact_uuid_new_'+date_time+'.csv', index=False)
+    print('Fact_UUIDs Successful')
+    end = time.time()
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
 
-    # # Converts UTC join_time and leave_time to PT timezone. Also removes missing join_time, leave_time, ip_address, meeting_id rows.
-    # incremental = df_master_data.copy()
-    # incremental = incremental.dropna(how='all')
-    # incremental = incremental.dropna(
-    #     axis=0, subset=['join_time', 'leave_time', 'ip_address', 'meeting_id'])
-    # # incremental['join_time'] = incremental.apply(lambda x: convert_to_pst(x['join_time']), axis = 1)
-    # # incremental['leave_time'] = incremental.apply(lambda x: convert_to_pst(x['leave_time']), axis = 1)
-    # # Create incremental index and sort by uuid and DID_Orig. Sorting by these variables is required to run the Uniques Algorithm successfully.
+    # Converts UTC join_time and leave_time to PT timezone. Also removes missing join_time, leave_time, ip_address, meeting_id rows.
+    incremental = df_master_data.copy()
+    incremental = incremental.dropna(how='all')
+    incremental = incremental.dropna(
+        axis=0, subset=['join_time', 'leave_time', 'ip_address', 'meeting_id'])
+    # incremental['join_time'] = incremental.apply(lambda x: convert_to_pst(x['join_time']), axis = 1)
+    # incremental['leave_time'] = incremental.apply(lambda x: convert_to_pst(x['leave_time']), axis = 1)
+    # Create incremental index and sort by uuid and DID_Orig. Sorting by these variables is required to run the Uniques Algorithm successfully.
 
-    # ########################################################################################################
-    # # Incremental Algorithm
-    # ########################################################################################################
-    # print("running uniques algorithm for incremental data..")
-    # startTime = datetime.now()
-    # inc = incremental
-    # common_list = ["Zoom user", "zoom user", "user_name",  "iPhone", "iPad", "Jai Guru",
-    #                "Yogananda", "John", "Jane", "Mary",  "Maria", "Paul",  "Redmi", "", " ", "  ", "   "]
-    # inc['location'] = inc.location.apply(str)
-    # inc['country'] = inc['location'].apply(
-    #     lambda st: st[st.rfind("(")+1:st.rfind(")")])
-    # inc.dropna(subset=['country'], inplace=True)
-    # inc.reset_index(drop=True, inplace=True)
-    # for i in range(0, len(inc)-1):
-    #     print(i)
-    #     print(inc.iloc[i, 2])
-    #     if (inc.loc[i, 'user_name'] in common_list):
-    #         inc.loc[i, 'user_name'] = str(inc.loc[i, 'user_name']) + "_" + str(
-    #             inc.loc[i, 'location']) + "_" + str(inc.loc[i, 'country'])
-    #     print("isnull")
-    #     if (pd.isnull(inc.loc[i, 'user_name'])):
-    #         inc.loc[i, 'username_country'] = str(
-    #             inc.loc[i, 'user_name']) + "_" + str(inc.loc[i, 'country'])
-    #     else:
-    #         inc.loc[i, 'username_country'] = str(
-    #             inc.loc[i, 'user_name']) + "_" + str(inc.loc[i, 'country'])
+    ########################################################################################################
+    # Incremental Algorithm
+    ########################################################################################################
+    print("running uniques algorithm for incremental data..")
+    startTime = datetime.now()
+    inc = incremental
+    common_list = ["Zoom user", "zoom user", "user_name",  "iPhone", "iPad", "Jai Guru",
+                   "Yogananda", "John", "Jane", "Mary",  "Maria", "Paul",  "Redmi", "", " ", "  ", "   "]
+    inc['location'] = inc.location.apply(str)
+    inc['country'] = inc['location'].apply(
+        lambda st: st[st.rfind("(")+1:st.rfind(")")])
+    inc.dropna(subset=['country'], inplace=True)
+    inc.reset_index(drop=True, inplace=True)
+    for i in range(0, len(inc)-1):
+        print(i)
+        print(inc.iloc[i, 2])
+        if (inc.loc[i, 'user_name'] in common_list):
+            inc.loc[i, 'user_name'] = str(inc.loc[i, 'user_name']) + "_" + str(
+                inc.loc[i, 'location']) + "_" + str(inc.loc[i, 'country'])
+        print("isnull")
+        if (pd.isnull(inc.loc[i, 'user_name'])):
+            inc.loc[i, 'username_country'] = str(
+                inc.loc[i, 'user_name']) + "_" + str(inc.loc[i, 'country'])
+        else:
+            inc.loc[i, 'username_country'] = str(
+                inc.loc[i, 'user_name']) + "_" + str(inc.loc[i, 'country'])
 
-    # inc["DID_orig"] = range(1, 1 + len(inc))
-    # inc['DID'] = inc['DID_orig']
-    # inc.sort_values(by=['uuid', 'DID_orig'], ascending=False,  inplace=True)
-    # inc.reset_index(inplace=True)
+    inc["DID_orig"] = range(1, 1 + len(inc))
+    inc['DID'] = inc['DID_orig']
+    inc.sort_values(by=['uuid', 'DID_orig'], ascending=False,  inplace=True)
+    inc.reset_index(inplace=True)
 
-    # for i in range(0, len(inc)-1):
-    #     print(i)
-    #     j = 1
-    #     while (inc.loc[i, 'uuid'] == inc.loc[i+j, 'uuid']):
-    #         if ((inc.loc[i, 'username_country'] == inc.loc[i+j, 'username_country'] and incremental.loc[i, 'username_country'] != "") or (inc.loc[i, 'email'] == inc.loc[i+j, 'email'] and inc.loc[i, 'email'] != "")):
-    #             inc.loc[i, 'DID'] = inc.loc[i+j, 'DID']
-    #         if (i+j == len(inc) - 1):
-    #             break
-    #         j += 1
+    for i in range(0, len(inc)-1):
+        print(i)
+        j = 1
+        while (inc.loc[i, 'uuid'] == inc.loc[i+j, 'uuid']):
+            if ((inc.loc[i, 'username_country'] == inc.loc[i+j, 'username_country'] and incremental.loc[i, 'username_country'] != "") or (inc.loc[i, 'email'] == inc.loc[i+j, 'email'] and inc.loc[i, 'email'] != "")):
+                inc.loc[i, 'DID'] = inc.loc[i+j, 'DID']
+            if (i+j == len(inc) - 1):
+                break
+            j += 1
 
-    # inc["DID"] = inc.groupby("uuid")["DID"].rank("dense").astype(int)
-    # inc['DIDx'] = inc['DID']
-    # inc.drop(['index', 'username_country', 'DID_orig'], axis=1, inplace=True)
-    # incremental_uniques = inc.copy()
-    # print("uniques algorithm finished in %d", datetime.now() - startTime)
+    inc["DID"] = inc.groupby("uuid")["DID"].rank("dense").astype(int)
+    inc['DIDx'] = inc['DID']
+    inc.drop(['index', 'username_country', 'DID_orig'], axis=1, inplace=True)
+    incremental_uniques = inc.copy()
+    print("uniques algorithm finished in %d", datetime.now() - startTime)
 
-    # # appends the update date as the last column for auditing purposes and to be used by incremental refresh in PBI.
-    # incremental_uniques['updated_at'] = date_time
+    # appends the update date as the last column for auditing purposes and to be used by incremental refresh in PBI.
+    incremental_uniques['updated_at'] = date_time
 
-    # incremental_uniques = incremental_uniques[['user_id', 'user_name', 'email', 'location', 'network_type', 'join_time', 'leave_time', 'leave_reason',
-    #                                            'status', 'meeting_id', 'uuid', 'country', 'DID', 'DIDx', 'updated_at']]
-    # incremental_uniques = incremental_uniques.rename(
-    #     columns={'location': 'user_location', 'status': 'user_status', 'email': 'user_email'})
-    # # Remove non ascii characters
-    # incremental_uniques['user_name'] = incremental_uniques['user_name'].str.encode(
-    #     'ascii', 'ignore').str.decode('ascii')
-    # incremental_uniques['user_location'] = incremental_uniques['user_location'].str.encode(
-    #     'ascii', 'ignore').str.decode('ascii')
-    # # Anonymize the User Name and User Email
-    # anon_reproduce = Anonymize(salt='82waOqOc00GBVVkMIRbq3wjAi')
-    # to_transform = ['user_name']
-    # dic = {"": 'blank0', " ": 'blank1', "  ": 'blank2', "   ": 'blank3'}
-    # incremental_uniques['user_name'] = incremental_uniques['user_name'].replace(
-    #     dic)
-    # incremental_uniques['leave_reason'] = incremental_uniques['leave_reason'].str.split(
-    #     '<br>Reason: ').str[1]
-    # incremental_uniques['leave_reason'] = incremental_uniques['leave_reason'].str.split(
-    #     '.').str[0]
-    # incremental_uniques.to_csv(
-    #     './zoom_etl/'+date_time+'/fact_connections_uniques_blanks_'+date_time+'.csv', index=False)
-    # anon_reproduce.transform(incremental_uniques, to_transform, verbose=True)
-    # # Save incremental data with unique IDs
-    # incremental_uniques.to_csv(
-    #     './zoom_etl/'+date_time+'/fact_connections_uniques_'+date_time+'.csv', index=False)
-    # incremental_uniques.to_csv(
-    #     './Production_Fact_Connections/fact_connections_uniques_'+date_time+'.csv', index=False)
-    # incremental_uniques.to_csv(
-    #     './Staging_Fact_Connections/fact_connection_uniques_increment.csv', index=False)
-    # ########################################################################################################
-    # # Incremental Algorithm Complete
-    # ########################################################################################################
+    incremental_uniques = incremental_uniques[['user_id', 'user_name', 'email', 'location', 'network_type', 'join_time', 'leave_time', 'leave_reason',
+                                               'status', 'meeting_id', 'uuid', 'country', 'DID', 'DIDx', 'updated_at']]
+    incremental_uniques = incremental_uniques.rename(
+        columns={'location': 'user_location', 'status': 'user_status', 'email': 'user_email'})
+    # Remove non ascii characters
+    incremental_uniques['user_name'] = incremental_uniques['user_name'].str.encode(
+        'ascii', 'ignore').str.decode('ascii')
+    incremental_uniques['user_location'] = incremental_uniques['user_location'].str.encode(
+        'ascii', 'ignore').str.decode('ascii')
+    # Anonymize the User Name and User Email
+    anon_reproduce = Anonymize(salt='82waOqOc00GBVVkMIRbq3wjAi')
+    to_transform = ['user_name']
+    dic = {"": 'blank0', " ": 'blank1', "  ": 'blank2', "   ": 'blank3'}
+    incremental_uniques['user_name'] = incremental_uniques['user_name'].replace(
+        dic)
+    incremental_uniques['leave_reason'] = incremental_uniques['leave_reason'].str.split(
+        '<br>Reason: ').str[1]
+    incremental_uniques['leave_reason'] = incremental_uniques['leave_reason'].str.split(
+        '.').str[0]
+    incremental_uniques.to_csv(
+        './zoom_etl/'+date_time+'/fact_connections_uniques_blanks_'+date_time+'.csv', index=False)
+    anon_reproduce.transform(incremental_uniques, to_transform, verbose=True)
+    # Save incremental data with unique IDs
+    incremental_uniques.to_csv(
+        './zoom_etl/'+date_time+'/fact_connections_uniques_'+date_time+'.csv', index=False)
+    incremental_uniques.to_csv(
+        './Production_Fact_Connections/fact_connections_uniques_'+date_time+'.csv', index=False)
+    incremental_uniques.to_csv(
+        './Staging_Fact_Connections/fact_connection_uniques_increment.csv', index=False)
+    print('Incremental Algorithm complete')
+    ########################################################################################################
+    # Incremental Algorithm Complete
+    ########################################################################################################
