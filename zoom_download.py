bdfcf05c0347fb0d6ddb34ef739c7e8319f69816
EC2 Default User <ec2-user@ip-10-0-2-5.us-east-2.compute.internal>
2025-08-30 18:07:50 +0000

    updates to zoom_download to support s3

--- zoom_download.py
+++ zoom_download.py
@@ -29,6 +29,7 @@ from zoomus import ZoomClient as RawClient
 from pyzoom import ZoomClient as PyZoomClient
 import json
 from sqlalchemy import create_engine
+import boto3
 
 if __name__ == "__main__":
     start = time.time()
@@ -86,8 +87,21 @@ if __name__ == "__main__":
     print('fact_uuid_new df shape:', fact_uuid_new.shape, fact_uuid_new.columns)
     fact_uuid_new.to_csv('./zoom_etl/'+date_time +
                          '/fact_uuid_new_filter_meeting_ids_'+date_time+'.csv', index=False)
-
-    fact_uuid_old = pd.read_csv('fact_uuid_master.csv', index_col=False)
+    
+    # load master fact UUID file
+    # fact_uuid_old = pd.read_csv('fact_uuid_master.csv', index_col=False)
+    bucket = 'omc-data'
+    s3_prefix = 'OMC_Services/Production_FU/fact_uuid_master'
+    client = boto3.client('s3', region_name=os.getenv('AWS_DEFAULT_REGION'), aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                          aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+    get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
+    objs = client.list_objects_v2(Bucket=bucket, Prefix=s3_prefix)['Contents']
+    fact_uuid_master_s3 = [obj['Key'] for obj in sorted(objs, key=get_last_modified)][0]
+    obj = client.get_object(Bucket= bucket, Key= fact_uuid_master_s3) 
+    
+    print('Fact UUID master:', fact_uuid_master_s3)
+    fact_uuid_old = pd.read_csv(obj['Body'], index_col=False) 
+    
     print('fact_uuid_old shape: ', fact_uuid_old.shape)
     # merge new and old fact uuid files and filter on _merge = Left_only to keep only the new uuids (latest extract's uuids)
     new_old_fact_uuid = fact_uuid_new.merge(
@@ -107,8 +121,11 @@ if __name__ == "__main__":
     print('pass')
     print('new_only_fact_uuid df shape and columns: ',
           new_only_fact_uuid.shape, new_only_fact_uuid.columns)
-    new_only_fact_uuid = new_only_fact_uuid.iloc[:, [
-        0, 1, 2, 3, 5, 7, 8, 9, 10]]
+    #new_only_fact_uuid = new_only_fact_uuid.iloc[:, [
+    #    0, 1, 2, 3, 5, 7, 8, 9, 10]]
+    cols = ['uuid', 'id', 'topic', 'user_name', 'user_email', 'start_time', 'end_time', 'duration', 'participants_count']
+    new_only_fact_uuid = new_only_fact_uuid[cols]
+    
     # print('df_uuid_new_only share and columns:', df_uuid_new_only.shape, df_uuid_new_only.columns)
     df_uuid_new_only = new_only_fact_uuid.reset_index(drop=True)
     df_uuid = new_only_fact_uuid.reset_index(drop=True)
